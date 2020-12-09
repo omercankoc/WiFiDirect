776d6bfbb16edb52affebdc47b8948710046e664
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2020-12-09 10:49:51 +0300

    Created

---
+++ Android/WiFiMessenger/app/src/main/java/com/carvio/wifimessenger/ActivityMessenger.java
@@ -0,0 +1,14 @@
+package com.carvio.wifimessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityMessenger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messenger);
+    }
+}
